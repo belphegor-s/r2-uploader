5534f5b1fad5f75929723bca1200039d8b93b3d9
Ayush Sharma <ayush2162020@gmail.com>
2025-05-10 13:07:05 +0530

    fix: Update NotFound component styles for improved visibility and user experience

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div className="relative z-10 text-center p-8 px-4 md:p-12 w-full min-h-screen flex items-center justify-center bg-gray-50">
+    <div className="relative z-10 text-center p-8 px-4 md:p-12 w-full min-h-screen flex items-center justify-center bg-[#272727] text-[#f5f5f5]">
       <div>
         <h1 className="text-6xl font-bold mb-4">404</h1>
         <p className="text-xl mb-6">Oops! The page you&apos;re looking for doesn&apos;t exist.</p>
